bffe3999f5aeb98206715559bfed5e0b03495628
tavgar <104298517+tavgar@users.noreply.github.com>
2023-03-12 14:35:44 +0100

    Update and rename Main.py to main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+import argparse
+
+# Parse arguments from user
+parser = argparse.ArgumentParser()
+parser.add_argument("--headers", type=str, required=False,
+                    help="custom headers in format: key1:value1,key2:value2")
+parser.add_argument("--filename", type=str, required=True,
+                    help="filename containing URLs")
+args = parser.parse_args()
+
+# Prompt user for payload and custom headers
+payload = "()<>\"';:+ TESTOOO001122"
+headers = {}
+header_input = args.headers
+if header_input:
+    for header in header_input.split(','):
+        key, value = header.split(':')
+        headers[key.strip()] = value.strip()
+
+# Define function to test a single URL
+def test_url(url):
+    # Split the URL into base URL and query parameters
+    url_parts = urllib.parse.urlsplit(url)
+    base_url = url_parts.scheme + "://" + url_parts.netloc + url_parts.path
+    query_params = urllib.parse.parse_qs(url_parts.query)
+
+    if query_params:
+        # Test each query parameter separately
+        for param in query_params:
+            param_value = query_params[param][0]
+            query_url = base_url + "?" + param + "=" + param_value
+            test_url(query_url)
+    else:
+        # Test the URL as it is
+        method = "GET" if not url_parts.query else "POST"
+        response = requests.request(method, url, data=payload, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find TESTOOO001122 in HTML code
+        test_string = "TESTOOO001122"
+        test_string_start_index = str(soup).find(test_string)
+
+        # Check for characters before and after TESTOOO001122
+        char_count = 0
+        reflected_chars = ""
+        for char in payload:
+            if char in str(soup)[test_string_start_index-1:test_string_start_index] or \
+               char in str(soup)[test_string_start_index+len(test_string):test_string_start_index+len(test_string)+1]:
+                char_count += 1
+                reflected_chars += char
+
+        # Check if at least 3 characters were reflected correctly
+        if char_count >= 3:
+            print("The URL {} passed the test. Reflected characters: {}".format(url, reflected_chars))
+            return True
+        else:
+            print("The URL {} failed the test. Reflected characters: {}".format(url, reflected_chars))
+            return False
+
+# Read URLs from file and test each one
+with open(args.filename, "r") as file:
+    urls = file.readlines()
+
+for url in urls:
+    url = url.strip()
+    test_url(url)
